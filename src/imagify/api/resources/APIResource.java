698e5229ba0457434ccb1e0a9fc9a859cce9e913
pavan-fk <pavank@flipkart.com>
2016-03-31 01:07:11 +0530

    resource

--- src/imagify/api/resources/APIResource.java
+++ src/imagify/api/resources/APIResource.java
@@ -1,12 +1,21 @@
 package imagify.api.resources;
 
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 
 /**
  * User: pavank
  * Date: 31/03/16
  * Time: 12:27 AM
  **/
-@Path("/")
+@Path("/imagify")
 public class APIResource {
+
+    @POST
+    @Produces("image/png")
+    public Response dummy(String text) {
+
+    }
 }
