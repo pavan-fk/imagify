a737310bca64cef1b9f6268c9d88d3e063ebbae3
pavan-fk <pavank@flipkart.com>
2016-04-02 13:21:38 +0530

    working copy

--- src/imagify/api/resources/APIResource.java
+++ src/imagify/api/resources/APIResource.java
@@ -1,9 +1,16 @@
 package imagify.api.resources;
 
+import javax.imageio.ImageIO;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 /**
  * User: pavank
@@ -14,8 +21,23 @@ import javax.ws.rs.core.Response;
 public class APIResource {
 
     @POST
+    @Consumes(MediaType.APPLICATION_JSON)
     @Produces("image/png")
-    public Response dummy(String text) {
-
+    public Response dummy(String tweet) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        BufferedImage bufferedImage = new BufferedImage(320, 200,
+                BufferedImage.TYPE_INT_RGB);
+        Graphics graphics = bufferedImage.getGraphics();
+        graphics.setColor(Color.WHITE);
+        graphics.fillRect(0, 0, 200, 50);
+        graphics.setColor(Color.BLACK);
+        graphics.setFont(new Font(Font.SERIF, Font.BOLD, 18));
+        graphics.drawString(tweet, 10, 25);
+        try {
+            ImageIO.write(bufferedImage, "jpg", baos);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return Response.ok(baos.toByteArray()).build();
     }
 }
