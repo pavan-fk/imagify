1f6d167b21ef0bd4660c53305c73d2f8fc5d89b9
pavan-fk <pavank@flipkart.com>
2016-04-02 14:25:23 +0530

    horizontal word wrap

--- src/imagify/api/resources/APIResource.java
+++ src/imagify/api/resources/APIResource.java
@@ -23,21 +23,59 @@ public class APIResource {
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces("image/png")
-    public Response dummy(String tweet) {
+    public Response dummy() {
+        String tweet = "a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z ";
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         BufferedImage bufferedImage = new BufferedImage(320, 200,
                 BufferedImage.TYPE_INT_RGB);
-        Graphics graphics = bufferedImage.getGraphics();
-        graphics.setColor(Color.WHITE);
-        graphics.fillRect(0, 0, 200, 50);
-        graphics.setColor(Color.BLACK);
-        graphics.setFont(new Font(Font.SERIF, Font.BOLD, 18));
-        graphics.drawString(tweet, 10, 25);
+//        Graphics graphics = bufferedImage.getGraphics();
+//        graphics.setColor(Color.WHITE);
+//        graphics.fillRect(0, 0, 200, 50);
+//        graphics.setColor(Color.BLACK);
+//        graphics.setFont(new Font(Font.SERIF, Font.BOLD, 18));
+//        graphics.drawString(tweet, 10, 25);
+
+
+        Graphics2D g2 = bufferedImage.createGraphics();
+
+        g2.setColor(Color.WHITE);
+        g2.setFont(new Font(Font.SERIF, Font.BOLD, 18));
+        drawer(g2, tweet, 1, 15, 320);
+//        g2.drawString(tweet, 50, 50);
         try {
             ImageIO.write(bufferedImage, "jpg", baos);
         } catch (IOException e) {
             e.printStackTrace();
         }
         return Response.ok(baos.toByteArray()).build();
+    }
+
+    public void drawer(Graphics g, String s, int x, int y, int width) {
+        // FontMetrics gives us information about the width,
+        // height, etc. of the current Graphics object's Font.
+        FontMetrics fm = g.getFontMetrics();
+
+        int lineHeight = fm.getHeight();
+
+        int curX = x;
+        int curY = y;
+
+        String[] words = s.split(" ");
+
+        for (String word : words) {
+            // Find out thw width of the word.
+            int wordWidth = fm.stringWidth(word + " ");
+
+            // If text exceeds the width, then move to next line.
+            if (curX + wordWidth >= x + width) {
+                curY += lineHeight;
+                curX = x;
+            }
+
+            g.drawString(word, curX, curY);
+
+            // Move over to the right for next word.
+            curX += wordWidth;
+        }
     }
 }
