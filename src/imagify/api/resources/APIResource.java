bb722a49b9d04283c1d15b4452e5518209cf3bd5
pavan-fk <pavank@flipkart.com>
2016-04-25 15:50:45 +0530

    Working copy of API
    Image size hardcoded to iPhone screen size

--- src/imagify/api/resources/APIResource.java
+++ src/imagify/api/resources/APIResource.java
@@ -1,10 +1,7 @@
 package imagify.api.resources;
 
 import javax.imageio.ImageIO;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.awt.*;
@@ -21,27 +18,19 @@ import java.io.IOException;
 public class APIResource {
 
     @POST
-    @Consumes(MediaType.APPLICATION_JSON)
     @Produces("image/png")
-    public Response dummy() {
-        String tweet = "a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z ";
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public Response dummy(@FormParam("tweet") String tweet) {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        BufferedImage bufferedImage = new BufferedImage(320, 200,
+        BufferedImage bufferedImage = new BufferedImage(320, 480,
                 BufferedImage.TYPE_INT_RGB);
-//        Graphics graphics = bufferedImage.getGraphics();
-//        graphics.setColor(Color.WHITE);
-//        graphics.fillRect(0, 0, 200, 50);
-//        graphics.setColor(Color.BLACK);
-//        graphics.setFont(new Font(Font.SERIF, Font.BOLD, 18));
-//        graphics.drawString(tweet, 10, 25);
-
 
         Graphics2D g2 = bufferedImage.createGraphics();
 
         g2.setColor(Color.WHITE);
         g2.setFont(new Font(Font.SERIF, Font.BOLD, 18));
         drawer(g2, tweet, 1, 15, 320);
-//        g2.drawString(tweet, 50, 50);
+
         try {
             ImageIO.write(bufferedImage, "jpg", baos);
         } catch (IOException e) {
@@ -50,15 +39,15 @@ public class APIResource {
         return Response.ok(baos.toByteArray()).build();
     }
 
-    public void drawer(Graphics g, String s, int x, int y, int width) {
+    public void drawer(Graphics2D g, String s, int startingX, int startingY, int totalWidth) {
         // FontMetrics gives us information about the width,
         // height, etc. of the current Graphics object's Font.
         FontMetrics fm = g.getFontMetrics();
 
         int lineHeight = fm.getHeight();
 
-        int curX = x;
-        int curY = y;
+        int curX = startingX;
+        int curY = startingY;
 
         String[] words = s.split(" ");
 
@@ -67,11 +56,10 @@ public class APIResource {
             int wordWidth = fm.stringWidth(word + " ");
 
             // If text exceeds the width, then move to next line.
-            if (curX + wordWidth >= x + width) {
+            if (curX + wordWidth >= startingX + totalWidth) {
                 curY += lineHeight;
-                curX = x;
+                curX = startingX;
             }
-
             g.drawString(word, curX, curY);
 
             // Move over to the right for next word.
